313547df87e2a3aa5fe96fdec5d90971efda3210
WaddyLeonvil <54086752+WaddyLeonvil@users.noreply.github.com>
2022-01-19 15:56:27 -0500

    Added input
    Added container to take in degree amount

--- src/Components/Trig.js
+++ src/Components/Trig.js
@@ -4,7 +4,9 @@ import './Trig.css';
 export function Trig() {
     return (
         <div id='trig-container'>
-            Trig Container
+            <p>Trig Container</p>
+            <label htmlFor="degree">Degrees: </label>
+            <input type="text" className='degree' id='degree' name='degree'/>
         </div>
     )
 }
