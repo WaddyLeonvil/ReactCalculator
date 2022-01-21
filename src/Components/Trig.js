01c969c42338aa2e5419f27baf8aa393824785e5
WaddyLeonvil <54086752+WaddyLeonvil@users.noreply.github.com>
2022-01-20 19:35:02 -0500

    Added change function for trig

--- src/Components/Trig.js
+++ src/Components/Trig.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Trig.css';
 
 export function Trig() {
+    const [degrees, setDegrees] = useState("0");
+
+    const handleChange = (e) => {
+        if (e.target.value.match("^[0-9.]*$") != null) {
+            setDegrees(e.target.value);
+        }
+    }
+
     return (
         <div id='trig-container'>
             <p>Trig Container</p>
             <label htmlFor="degree">Degrees: </label>
-            <input type="text" className='degree' id='degree' name='degree'/>
+            <input type="text" value={degrees} name='degree' pattern='[0-9]' onChange={handleChange} />
+            <p>{degrees}</p>
+
+
         </div>
     )
 }
